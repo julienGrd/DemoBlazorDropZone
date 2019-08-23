7bf82681aa48f1896c32def2a0e05e2168a70327
julien gourdon <jul.gourdon@gmail.com>
2019-08-23 19:28:59 +0200

    Ajoutez des fichiers projet.

---
+++ DemoBlazorDropZone/Client/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿var indexComponent;
+var myDropzone;
+function initDropZone(pIndexComponent) {
+    indexComponent = pIndexComponent;
+    if (!myDropzone) {
+        myDropzone = new Dropzone("div.my-dropzone", { url: "/fake" });//we don't want using automatic post but library need url
+        Dropzone.forElement("div.my-dropzone").options.autoProcessQueue = false;//we don't want using automatic post
+    }
+    
+    //Dropzone.forElement("div.drop-zone").options.previewsContainer = false;
+    myDropzone.off("addedfile");
+    myDropzone.on("addedfile", function (file) {
+        let reader = new FileReader();
+        $("#uploadFileLoader").show();
+        reader.file = file;
+        reader.onload = function (event) {
+
+            //console.log(event.target.result);
+            var base64 = reader.result.split(',')[1];
+
+            console.log(base64.length);
+            indexComponent.invokeMethodAsync('UploadFile', base64)
+            .then((data) => {
+                myDropzone.removeFile(reader.file);
+
+                $("#uploadFileLoader").hide();
+            })
+            .catch((error) => {
+                alert("error : " + error.toString());
+                $("#uploadFileLoader").hide();
+                if (reader.file) {
+                    myDropzone.removeFile(reader.file);
+                }
+
+            });
+
+
+        };
+        reader.readAsDataURL(file);
+
+
+    });
+}
